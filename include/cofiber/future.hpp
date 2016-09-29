0290fa20989ea1e17c89c15350e9b9d58fbc0271
Alexander van der Grinten <alexander.vandergrinten@gmail.com>
2016-09-29 20:05:52 +0000

    Allow non-void returning futures

--- include/cofiber/future.hpp
+++ include/cofiber/future.hpp
@@ -7,48 +7,59 @@
 namespace cofiber {
 
 namespace _future {
+	// ------------------------------------------------------------------------
+	// Internal shared state.
+	// ------------------------------------------------------------------------
+
 	enum {
 		has_value = 1,
 		has_functor = 2
 	};
-
-	template<typename T>
-	struct state_struct {
-		state_struct()
+	
+	struct state_base {
+		state_base()
 		: refs(1), status(0) { }
 
 		std::atomic<int> refs;
 		std::atomic<int> status;
-//		std::aligned_storage<sizeof(T), alignof(T)> storage;
 		std::function<void()> functor;
 	};
+
+	template<typename T>
+	struct state_struct : state_base {
+		std::aligned_storage<sizeof(T), alignof(T)> storage;
+	};
+
+	template<>
+	struct state_struct<void> : state_base { };
+	
+	// ------------------------------------------------------------------------
+	// future class.
+	// ------------------------------------------------------------------------
 	
 	template<typename T>
 	struct promise;
-
-	template<typename T>
-	struct future {
-		friend class promise<T>;
-
-		friend void swap(future &a, future &b) {
+	
+	struct future_base {
+		friend void swap(future_base &a, future_base &b) {
 			using std::swap;
 			swap(a._state, b._state);
 		}
 
-		future(const future &) = delete;
+		future_base(const future_base &) = delete;
 		
-		future(future &&other) {
+		future_base(future_base &&other) {
 			swap(*this, other);
 		}
 
-		~future() {
+		~future_base() {
 			if(_state->refs.fetch_sub(1, std::memory_order_release) == 1) {
 				std::atomic_thread_fence(std::memory_order_acquire);
 				delete _state;
 			}
 		}
 
-		future &operator= (future other) {
+		future_base &operator= (future_base other) {
 			swap(*this, other);
 			return *this;
 		}
@@ -71,50 +82,101 @@ namespace _future {
 				handle.resume();
 			});
 		}
-
-		void await_resume() {
-
-		}
-	private:
-		explicit future(state_struct<T> *state)
+		
+	protected:
+		explicit future_base(state_base *state)
 		: _state(state) {
 			_state->refs.fetch_add(1, std::memory_order_relaxed);
 		}
 
-		state_struct<T> *_state;
+		state_base *_state;
 	};
 
 	template<typename T>
-	struct promise {
-		friend void swap(promise &a, promise &b) {
+	struct future : future_base {
+		friend class promise<T>;
+
+		T await_resume() {
+			auto derived = static_cast<state_struct<T> *>(_state);
+			return std::move(*reinterpret_cast<T *>(&derived->storage));
+		}
+
+	private:
+		explicit future(state_struct<T> *state)
+		: future_base(state) { }
+	};
+
+	template<>
+	struct future<void> : future_base {
+		friend class promise<void>;
+
+		void await_resume() { }
+
+	private:
+		explicit future(state_struct<void> *state)
+		: future_base(state) { }
+	};
+	
+	// ------------------------------------------------------------------------
+	// promise class.
+	// ------------------------------------------------------------------------
+
+	struct promise_base {
+		friend void swap(promise_base &a, promise_base &b) {
 			using std::swap;
 			swap(a._state, b._state);
 		}
 		
-		promise(const promise &) = delete;
+		promise_base(const promise_base &) = delete;
 		
-		promise(promise &&other) {
+		promise_base(promise_base &&other) {
 			swap(*this, other);
 		}
 
-		~promise() {
+		~promise_base() {
 			if(_state->refs.fetch_sub(1, std::memory_order_release) == 1) {
 				std::atomic_thread_fence(std::memory_order_acquire);
 				delete _state;
 			}
 		}
 
-		promise &operator= (promise other) {
+		promise_base &operator= (promise_base other) {
 			swap(*this, other);
 			return *this;
 		}
 
-		promise()
-		: _state(new state_struct<T>) { }
+	protected:
+		explicit promise_base(state_base *state)
+		: _state(state) { }
 
-		future<T> get_future() {
-			return future<T>(_state);
+		state_base *_state;
+	};
+	
+	template<typename T>
+	struct promise : promise_base {
+		promise()
+		: promise_base(new state_struct<T>) { }
+
+		void set_value(T value) {
+			auto derived = static_cast<state_struct<T> *>(_state);
+			new (&derived->storage) T(std::move(value));
+
+			int s = _state->status.fetch_or(has_value);
+			assert(!(s & has_value));
+			if(s & has_functor)
+				_state->functor();
 		}
+		
+		future<T> get_future() {
+			auto derived = static_cast<state_struct<T> *>(_state);
+			return future<T>(derived);
+		}
+	};
+	
+	template<>
+	struct promise<void> : promise_base {
+		promise()
+		: promise_base(new state_struct<void>) { }
 
 		void set_value() {
 			int s = _state->status.fetch_or(has_value);
@@ -122,9 +184,11 @@ namespace _future {
 			if(s & has_functor)
 				_state->functor();
 		}
-
-	private:
-		state_struct<T> *_state;
+		
+		future<void> get_future() {
+			auto derived = static_cast<state_struct<void> *>(_state);
+			return future<void>(derived);
+		}
 	};
 };
 
@@ -141,8 +205,9 @@ struct coroutine_traits<future<T>> {
 		auto initial_suspend() { return suspend_never(); }
 		auto final_suspend() { return suspend_never(); }
 
-		void return_value() {
-			_promise.set_value();
+		template<typename... V>
+		void return_value(V &&... value) {
+			_promise.set_value(std::forward<V>(value)...);
 		}
 
 	private:
--- tests/future.cpp
+++ tests/future.cpp
@@ -4,7 +4,13 @@
 #include <cofiber.hpp>
 #include <cofiber/future.hpp>
 
+COFIBER_ROUTINE(cofiber::future<int>, h(), [] () {
+	COFIBER_RETURN(42);
+});
+
 COFIBER_ROUTINE(cofiber::future<void>, f(), [] () {
+	auto x = COFIBER_AWAIT h();
+	std::cout << "h() returned " << x << std::endl;
 	COFIBER_RETURN();
 });
 
